a78383ebdf1c814d40f8d7756b5ad073978ce578
James <ball.j.work@gmail.com>
2021-11-08 13:26:19 +0000

    Updates

---
+++ precache-manifest.fc0a3405dbc1ab89c7480b94cfa359d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df57f69f21f576afb5f4c864afcde58",
+    "url": "/jumboindex/flashcards/index.html"
+  },
+  {
+    "revision": "295a347f344e768c0950",
+    "url": "/jumboindex/flashcards/static/js/2.80bf8026.chunk.js"
+  },
+  {
+    "revision": "6246b5e6349feaa9bd08083d2e4d002f",
+    "url": "/jumboindex/flashcards/static/js/2.80bf8026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbb7de44db3ccf34393",
+    "url": "/jumboindex/flashcards/static/js/main.29d41f7c.chunk.js"
+  },
+  {
+    "revision": "09eedc075e549c9afb78",
+    "url": "/jumboindex/flashcards/static/js/runtime-main.9b09c263.js"
+  }
+]);
